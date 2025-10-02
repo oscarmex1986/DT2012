a8fbb3ffd7e52cadfc2be306f5678d5bcd85c290
oscarmex1986 <oscar.molina@hh.se>
2025-10-02 11:01:58 +0200

    Create DoubleExpressions.java

---
+++ Week-1/DoubleExpressions.java
@@ -0,0 +1,9 @@
+
+public class DoubleExpressions {
+    public static void main(String[] args) {
+        double x = 5.5, y = 2.2;
+        System.out.println("Addition: " + (x + y));
+        System.out.println("Multiplication: " + (x * y));
+        System.out.println("Random number (0 to 1): " + Math.random());
+    }
+}
