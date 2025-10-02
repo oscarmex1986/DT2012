938491541f1e95494fcae4adb11fc8897544f27b
oscarmex1986 <oscar.molina@hh.se>
2025-10-02 11:09:24 +0200

    Create StringOperations.java

---
+++ Week-1/StringOperations.java
@@ -0,0 +1,10 @@
+public class StringOperations {
+    public static void main(String[] args) {
+        String first = "Hello";
+        String second = "World";
+        String combined = first + " " + second;
+        System.out.println("Concatenation: " + combined);
+        System.out.println("Length: " + combined.length());
+        System.out.println("Uppercase: " + combined.toUpperCase());
+    }
+}
