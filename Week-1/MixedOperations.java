777ed131ce2c2ff445acd86a156918d1ee40971b
oscarmex1986 <oscar.molina@hh.se>
2025-10-02 11:11:37 +0200

    Create MixedOperations.java

---
+++ Week-1/MixedOperations.java
@@ -0,0 +1,9 @@
+public class MixedOperations {
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 1.75;
+        String name = "Alice";
+      
+        System.out.println(name + " is " + age + " years old and " + height + " meters tall.");
+    }
+}
