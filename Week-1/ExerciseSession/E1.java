2f40702114dbaf87e4fbfc8a2b290b1c97d7b7d7
oscarmex1986 <oscar.molina@hh.se>
2025-10-07 13:30:34 +0200

    Create E1.java

---
+++ Week-1/ExerciseSession/E1.java
@@ -0,0 +1,15 @@
+/*
+
+Add code to print one more line with the message
+   Welcome to the programming course!
+after the Hello, world! message.
+
+*/
+public class E1{
+
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+    // Your code here!
+  }
+
+}
